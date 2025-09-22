ba5ea4ec83a76f8eee9c05da61bc778fc6b39d97
prathyusha-thogunta <thogunta.prathyusha@cognizant.com>
2025-09-22 10:32:29 +0530

    1st user story-prathyusha

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { TravelInsurancePage } from '../support/pageobjectmodel/FamilyInsurance';
+
+describe('Travel Insurance Test Suite', () => {
+  const travelPage = new TravelInsurancePage();
+
+  it('Find student travel insurance for 2 people (Europe), display 3 lowest plans', () => {
+    cy.on('uncaught:exception', () => false);
+
+    //1st user story
+    travelPage.visitHomePage();
+
+    travelPage.navigateToTravelInsurance();
+
+    travelPage.selectSingleTripOption();
+
+  });
+});
