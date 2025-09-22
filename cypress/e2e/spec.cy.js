aa52c1748355488db4eae035491c92655ddf6ba5
BhavanaBhushamoni <bhavana.bhushamoni@cognizant.com>
2025-09-22 10:57:39 +0530

    first user story-Bhavana

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,24 @@
 import { TravelInsurancePage } from '../support/pageobjectmodel/FamilyInsurance';
+import { HealthInsurancePage } from '../support/pageobject/HealthInsurance';
 
 describe('Travel Insurance Test Suite', () => {
-  const travelPage = new TravelInsurancePage();
+  travelPage = new TravelInsurancePage();
 
   it('Find student travel insurance for 2 people (Europe), display 3 lowest plans', () => {
     cy.on('uncaught:exception', () => false);
 
     //1st user story
     travelPage.visitHomePage();
-
     travelPage.navigateToTravelInsurance();
-
     travelPage.selectSingleTripOption();
 
+
+
+
+    it('Story 12: G-H-V Flow - Access Health Insurance from General dropdown', () => {
+    HealthInsurancePage.expandGeneralDropdown();
+    HealthInsurancePage.waitForDropdownVisible();
+    HealthInsurancePage.clickHealthInsuranceLabel();
+  });
   });
 });
